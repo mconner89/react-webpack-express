1b388898d00b3f7fd70d5343053bcea97a7d572e
ptbarnum4 <peterbarnum@gmail.com>
2020-03-07 22:17:48 -0700

    made a couple changes for starter that I find myself doing every time

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledH1 = styled.h1`
-  font-size: 48px;
-  color: dodgerblue;
-  font-family: fantasy;
-  :hover {
-    color: limegreen;
-    cursor: crosshair;
-    user-select: none;
-    text-shadow: 0 0 5px black;
-  }
-  :active {
-    transform: scale(0.9, 0.9)
-  }
-`
+
 
 class App extends React.Component {
   constructor(props) {
@@ -24,11 +11,49 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <StyledH1>
-          This is a styled header
-        </StyledH1>
-      </div>
+      <React.Fragment>
+        <nav>
+          <a href='#link1'>Link 1</a>
+          <a href='#link2'>Link 2</a>
+          <a href='#link3'>Link 3</a>
+        </nav>
+
+        <div className='page'>
+          <div className='section'>
+            <h1>
+              Hello from React / Webpack / Express
+            </h1>
+            <p>
+              I'm baby shaman selvage 3 wolf moon plaid meh chambray. Tumblr tacos occupy bushwick everyday carry flannel pop-up plaid 8-bit polaroid live-edge locavore. Tote bag cray sartorial umami, yr 90's intelligentsia 8-bit. Succulents fanny pack sustainable austin. Artisan godard chicharrones vaporware af leggings gentrify unicorn.
+            </p>
+            <p>
+              Health goth kickstarter XOXO polaroid, mumblecore pickled bicycle rights hexagon scenester 8-bit plaid synth franzen. Tumblr hashtag salvia, tumeric palo santo organic selvage tilde deep v freegan cloud bread. Retro etsy tofu brunch knausgaard slow-carb green juice polaroid, yr you probably haven't heard of them vape four dollar toast selvage. Etsy tumblr organic hammock bicycle rights fanny pack.
+
+            </p>
+            <p>
+              Waistcoat jean shorts offal air plant, 8-bit butcher squid vinyl celiac YOLO asymmetrical marfa. IPhone hella gastropub, shabby chic hoodie literally everyday carry pork belly umami tote bag blog pitchfork shoreditch. Plaid tbh echo park, pork belly pour-over pop-up polaroid umami salvia flexitarian bitters gentrify raw denim bespoke. Sustainable blog asymmetrical drinking vinegar sriracha artisan. Slow-carb freegan migas raw denim, fashion axe succulents drinking vinegar.
+
+            </p>
+            <p>
+              Raclette chambray sustainable meggings humblebrag salvia, swag affogato pabst XOXO 90's 3 wolf moon blue bottle drinking vinegar brooklyn. Fingerstache pug mustache chartreuse, vinyl meggings portland semiotics twee jean shorts gochujang. Taiyaki adaptogen poke pabst 90's yr enamel pin live-edge tofu. Godard mixtape ugh synth cred offal pop-up thundercats food truck pok pok.
+            </p>
+            <p>
+              Umami portland chartreuse bespoke fashion axe raclette single-origin coffee crucifix. Viral tofu put a bird on it waistcoat edison bulb godard master cleanse schlitz man bun scenester slow-carb. Selvage tofu vegan sustainable offal. Keffiyeh 3 wolf moon yr cornhole, portland intelligentsia vegan twee lumbersexual polaroid. Fam paleo cliche green juice tousled tote bag.
+            </p>
+              Edison bulb gentrify echo park pork belly church-key banjo. Green juice copper mug lumbersexual, health goth schlitz ethical knausgaard small batch mixtape. Knausgaard cold-pressed chicharrones, blog 8-bit wolf DIY sriracha poke chia letterpress gochujang. 3 wolf moon biodiesel keytar pitchfork normcore squid microdosing vinyl gochujang ennui migas distillery. Retro tbh palo santo, man braid keytar chicharrones authentic marfa hammock craft beer. Lomo iceland williamsburg bicycle rights typewriter. 90's gochujang flannel vape tumeric, helvetica tbh.
+            <p>
+              Iceland typewriter squid slow-carb fixie. Yuccie jean shorts fixie butcher tbh. Echo park cold-pressed kitsch vaporware banjo gastropub helvetica whatever kickstarter artisan jianbing. DIY bitters forage deep v seitan sartorial church-key salvia waistcoat cronut leggings four dollar toast YOLO brunch pitchfork.
+
+            </p>
+            <p>
+              Vape brunch jianbing iPhone. Food truck everyday carry celiac, palo santo cold-pressed XOXO hammock tousled lyft. Keffiyeh lo-fi skateboard 8-bit, selvage whatever roof party art party hella hashtag next level waistcoat plaid. Tousled polaroid microdosing mumblecore pork belly readymade. Hoodie scenester subway tile cornhole synth, lumbersexual humblebrag swag roof party banh mi microdosing taxidermy. Chambray 90's locavore swag succulents lyft food truck man braid vaporware sustainable af wayfarers tilde poutine.
+            </p>
+            <p>
+              Chartreuse church-key four loko affogato live-edge palo santo brooklyn kombucha DIY. Everyday carry bicycle rights mlkshk adaptogen, +1 single-origin coffee cardigan beard pop-up cronut. Whatever pop-up hoodie occupy cliche knausgaard, gochujang paleo sriracha 90's. Af pork belly bitters four loko.
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -8,11 +8,70 @@ import { createGlobalStyle } from 'styled-components'
    at the top-level render*/
 
   const GlobalStyle = createGlobalStyle`
+  ::-webkit-scrollbar {
+    width: .5em;
+  }
+  ::-webkit-scrollbar-track {
+    background: linear-gradient(90deg, #FFF 0%, #000 100%);
+  }
+  ::-webkit-scrollbar-thumb {
+    background: linear-gradient(90deg, #FFF 0%, #00E150 100%);
+    border-radius: 2px;
+  }
+  * {
+    font-family: 'Rokkitt', serif;
+    word-spacing: .1em;
+  }
+  html {
+    font-size: calc(1em + .70vw);
+    @media screen and (min-width: 70em) {
+      font-size: 1.4em;
+    }
+  }
   body {
     display: flex;
     justify-content: center;
+  }
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60px;
+
+    display: flex;
     align-items: center;
-    height: 100vh;
+
+    background-color: #ffffff;
+    box-shadow: 0 0 3px #191919;
+    a {
+      color: #191919;
+      text-decoration: none;
+      margin: 0 2vw;
+      padding: 6px 1vw;
+      :hover {
+        background-color: #F0F0F0;
+      }
+    }
+  }
+  p {
+    white-space: auto;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Fresca', sans-serif;
+  }
+  .page {
+    padding-top: 60px;
+    max-width: 1200px;
+  }
+  .section {
+    margin: 0 auto;
+    width: 80%;
+  }
+  @media screen and (max-width: 640px) {
+    .section {
+      width: 94%;
+    }
   }
 `
 
